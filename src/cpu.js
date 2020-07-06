5c8b05ebe50f41e7ac430fc6069007a79fb5bb50
adamkgray <kruschka.kruschka@gmail.com>
2020-07-06 12:28:06 +0800

    use switch syntax

--- src/cpu.js
+++ src/cpu.js
@@ -20,92 +20,135 @@ class Cpu {
 
     decodeOpcode() {
         /* opcodes are classified mostly by the leftmost 4 bits, which you can isolate with 0xF000 */
-        if ((this.opcode & 0xF000) == 0x0000) {
-            if (this.opcode == 0x00E0) {
-                this.instruction = "00E0";
-            } else if (this.opcode == 0x00EE) {
-                this.instruction = "00EE";
-            } else {
-                this.instruction = "0NNN";
-            }
-        } else if ((this.opcode & 0xF000) == 0x1000) {
-            this.instruction = "1NNN";
-        } else if ((this.opcode & 0xF000) == 0x2000) {
-            this.instruction = "2NNN";
-        } else if ((this.opcode & 0xF000) == 0x3000) {
-            this.instruction = "3XNN";
-        } else if ((this.opcode & 0xF000) == 0x4000) {
-            this.instruction = "4XNN";
-        } else if ((this.opcode & 0xF000) == 0x5000) {
-            this.instruction = "5XY0";
-        } else if ((this.opcode & 0xF000) == 0x6000) {
-            this.instruction = "6XNN";
-        } else if ((this.opcode & 0xF000) == 0x7000) {
-            this.instruction = "7XNN";
-        } else if ((this.opcode & 0xF000) == 0x8000) {
-            if ((this.opcode & 0xF) == 0x0) {
-                this.instruction = "8XY0";
-            } else if ((this.opcode & 0xF) == 0x1) {
-                this.instruction = "8XY1";
-            } else if ((this.opcode & 0xF) == 0x2) {
-                this.instruction = "8XY2";
-            } else if ((this.opcode & 0xF) == 0x3) {
-                this.instruction = "8XY3";
-            } else if ((this.opcode & 0xF) == 0x4) {
-                this.instruction = "8XY4";
-            } else if ((this.opcode & 0xF) == 0x5) {
-                this.instruction = "8XY5";
-            } else if ((this.opcode & 0xF) == 0x6) {
-                this.instruction = "8XY6";
-            } else if ((this.opcode & 0xF) == 0x7) {
-                this.instruction = "8XY7";
-            } else if ((this.opcode & 0xF) == 0xE) {
-                this.instruction = "8XYE";
-            } else {
-                throw (`FATAL ERROR: unknown opcode ${opcode} in 8???`)
-            }
-        } else if ((opcode & 0xF000) == 0x9000) {
-            this.instruction = "9XY0";
-        } else if ((this.opcode & 0xF000) == 0xA000) {
-            this.instruction = "ANNN";
-        } else if ((this.opcode & 0xF000) == 0xB000) {
-            this.instruction = "BNNN";
-        } else if ((this.opcode & 0xF000) == 0xC000) {
-            this.instruction = "CXNN";
-        } else if ((this.opcode & 0xF000) == 0xD000) {
-            this.instruction = "DXYN";
-        } else if ((this.opcode & 0xF000) == 0xE000) {
-            if ((this.opcode & 0xFF) == 0x9E) {
-                this.instruction = "EX9E";
-            } else if ((this.opcode & 0xFF) == 0xA1) {
-                this.instruction = "EXA1";
-            } else {
-                throw (`FATAL ERROR: unknown opcode ${opcode} in E???`)
-            }
-        } else if ((this.opcode & 0xF000) == 0xF000) {
-            if ((this.opcode & 0xFF) == 0x07) {
-                this.instruction = "FX07";
-            } else if ((this.opcode & 0xFF) == 0x0A) {
-                this.instruction = "FX0A";
-            } else if ((this.opcode & 0xFF) == 0x15) {
-                this.instruction = "FX15";
-            } else if ((this.opcode & 0xFF) == 0x18) {
-                this.instruction = "FX18";
-            } else if ((this.opcode & 0xFF) == 0x1E) {
-                this.instruction = "FX1E";
-            } else if ((this.opcode & 0xFF) == 0x29) {
-                this.instruction = "FX29";
-            } else if ((this.opcode & 0xFF) == 0x33) {
-                this.instruction = "FX33";
-            } else if ((this.opcode & 0xFF) == 0x55) {
-                this.instruction = "FX55";
-            } else if ((this.opcode & 0xFF) == 0x65) {
-                this.instruction = "FX65";
-            } else {
-                throw (`FATAL ERROR: unknown opcode ${opcode} in F???`)
-            }
-        } else {
-            throw (`FATAL ERROR: unknown opcode ${opcode} (matched no pattern)`)
+        switch (this.opcode & 0xF000) {
+            case 0x0000:
+                switch (this.opcode) {
+                    case 0x00E0:
+                        this.instruction = "00E0";
+                        break;
+                    case 0x0EE:
+                        this.instruction = "00EE";
+                        break;
+                    default:
+                        this.instruction = "0NNN";
+                        break;
+                }
+            case 0x1000:
+                this.instruction = "1NNN";
+                break;
+            case 0x2000:
+                this.instruction = "2NNN";
+                break;
+            case 0x3000:
+                this.instruction = "3XNN";
+                break;
+            case 0x4000:
+                this.instruction = "4XNN";
+                break;
+            case 0x5000:
+                this.instruction = "5XY0";
+                break;
+            case 0x6000:
+                this.instruction = "6XNN";
+                break;
+            case 0x7000:
+                this.instruction = "7XNN";
+                break;
+            case 0x8000:
+                switch (this.opcode & 0xF) {
+                    case 0x0:
+                        this.instruction = "8XY0";
+                        break;
+                    case 0x1:
+                        this.instruction = "8XY1";
+                        break;
+                    case 0x2:
+                        this.instruction = "8XY2";
+                        break;
+                    case 0x3:
+                        this.instruction = "8XY3";
+                        break;
+                    case 0x4:
+                        this.instruction = "8XY4";
+                        break;
+                    case 0x5:
+                        this.instruction = "8XY5";
+                        break;
+                    case 0x6:
+                        this.instruction = "8XY6";
+                        break;
+                    case 0x7:
+                        this.instruction = "8XY7";
+                        break;
+                    case 0xE:
+                        this.instruction = "8XYE";
+                        break;
+                    default:
+                        throw (`FATAL ERROR: unknown opcode '${opcode}' like 8XXX`);
+                        break;
+                }
+            case 0x9000:
+                this.instruction = "9XY0";
+                break;
+            case 0xA000:
+                this.instruction = "ANNN";
+                break;
+            case 0xB000:
+                this.instruction = "BNNN";
+                break;
+            case 0xC000:
+                this.instruction = "CXNN";
+                break;
+            case 0xD000:
+                this.instruction = "DXYN";
+                break;
+            case 0xE000:
+                switch (this.opcode & 0xFF) {
+                    case 0x9E:
+                        this.instruction = "EX9E";
+                    case 0xA1:
+                        this.instruction = "EXA1";
+                    default:
+                        throw (`FATAL ERROR: unknown opcode '${opcode}' like EXXX`);
+                        break;
+                }
+            case 0xF000:
+                switch (this.opcode & 0xFF) {
+                    case 0x07:
+                        this.instruction = "FX07";
+                        break;
+                    case 0x0A:
+                        this.instruction = "FX0A";
+                        break;
+                    case 0x15:
+                        this.instruction = "FX15";
+                        break;
+                    case 0x18:
+                        this.instruction = "FX18";
+                        break;
+                    case 0x1E:
+                        this.instruction = "FX1E";
+                        break;
+                    case 0x29:
+                        this.instruction = "FX29";
+                        break;
+                    case 0x33:
+                        this.instruction = "FX33";
+                        break;
+                    case 0x55:
+                        this.instruction = "FX55";
+                        break;
+                    case 0x65:
+                        this.instruction = "FX65";
+                        break;
+                    default:
+                        throw (`FATAL ERROR: unknown opcode '${opcode}' like FXXX`);
+                        break;
+
+                }
+            default:
+                throw (`FATAL ERROR: unknown opcode '${opcode}' (no  matched pattern)`)
+                break;
         }
     }
 
